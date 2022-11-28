26bab2ec430e6c54b9e0b9cd613ab4a4574541e8
Segundo Salsamendi <segundosalsamendi8@gmail.com>
2022-11-28 19:43:12 -0300

    agrega event binding y control de stock

--- src/app/articles-list/articles-list.component.ts
+++ src/app/articles-list/articles-list.component.ts
@@ -14,29 +14,43 @@ export class ArticlesListComponent {
       category : 'Celulares',
       price : 1000,
       quantity : 15,
-      image : 'assets/img/iphone.png'
+      image : 'assets/img/iphone.png',
+      addproduct : 0,
     },
     {
       name : 'Smart Tv Samsung',
       category : 'Televisores',
       price : 500,
       quantity : 5,
-      image : 'assets/img/televisor.jpg'
+      image : 'assets/img/televisor.jpg',
+      addproduct : 0,
     },
     {
       name : 'Tablet Samsung',
       category : 'Tablets',
       price : 200,
       quantity : 1,
-      image : 'assets/img/tablet.jpg'
+      image : 'assets/img/tablet.png',
+      addproduct : 0,
     },
     {
       name : 'Monitor',
       category : 'Monitores',
       price : 150,
       quantity : 0,
-      image : 'assets/img/monitor.jpg'
+      image : 'assets/img/monitor.jpg',
+      addproduct : 0,
     }
   ];
-  
+ 
+  upProduct(article: article): void {
+    if (article.quantity > article.addproduct)
+      article.addproduct++;
+  }
+
+  downProduct(article: article): void {
+    if (article.addproduct > 0)
+      article.addproduct--;
+  }
+
 }
--- src/app/articles-list/articles.ts
+++ src/app/articles-list/articles.ts
@@ -4,4 +4,5 @@ export interface article {
     price: number;
     quantity: number;
     image: string;
+    addproduct: number;
 }
